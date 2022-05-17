d6f31bc353cd0201befdfe9da267b9823eae809a
defaultex <default_ex@live.com>
2022-05-17 18:17:31 -0400

    Implemented the utility functions, began on transform feedback functions.

--- hgl/src/hgl.transform_feedback.cs
+++ hgl/src/hgl.transform_feedback.cs
@@ -1,5 +1,9 @@
 partial class hgl {
-    // glBeginTransformFeedback
+
+    public static void BeginTransformFeedback(PrimitiveType mode) {
+        unsafe { gl.Functions.glBeginTransformFeedback(mode); }
+    }
+
     // glBindTransformFeedback
     // glCreateTransformFeedbacks
     // glDeleteTransformFeedbacks
@@ -7,14 +11,41 @@ partial class hgl {
     // glDrawTransformFeedbackInstanced
     // glDrawTransformFeedbackStream
     // glDrawTransformFeedbackStreamInstanced
-    // glEndTransformFeedback
+
+    public static void EndTransformFeedback() {
+        unsafe { gl.Functions.glEndTransformFeedback(); }
+    }
+
     // glGenTransformFeedbacks
     // glGetTransformFeedback
-    // glGetTransformFeedbackVarying
+
+    public static void GetTransformFeedbackVarying(GLprogram program, int index, int bufSize, out GLsizei size, out AttributeType type, out string name) {
+        unsafe {
+            GLsizei length = bufSize;
+            GLchar* cstring = stackalloc GLchar[bufSize];
+            fixed (GLint* psize = &size) fixed (AttributeType* ptype = &type) {
+                gl.Functions.glGetTransformFeedbackVarying(program, (GLuint)index, bufSize, &length, psize, ptype, cstring);
+            }
+            name = Encoding.UTF8.GetString(cstring, length);
+        }
+    }
+
     // glIsTransformFeedback
     // glPauseTransformFeedback
     // glResumeTransformFeedback
     // glTransformFeedbackBufferBase
     // glTransformFeedbackBufferRange
-    // glTransformFeedbackVaryings
+
+    // TODO : validate this method actually works, have a strong feeling it doesn't
+    public static void glTransformFeedbackVaryings(GLprogram program, int count, string[] varyings, TransformFeedbackBufferMode bufferMode) {
+        unsafe {
+            GLchar** varray = stackalloc GLchar*[varyings.Length];
+            for (int i = 0; i < varyings.Length; i++) {
+                // note: doing a stackalloc here says 'unable to convert byte* to byte*' as there are two unique 'byte*' types
+                byte[] vstr = Encoding.UTF8.GetBytes(varyings[i]);
+                fixed (GLchar* vstrPtr = vstr) varray[i] = vstrPtr;
+            }
+            gl.Functions.glTransformFeedbackVaryings(program, (GLsizei)count, varray, bufferMode);
+        }
+    }
 }
--- hgl/src/hgl.utility.cs
+++ hgl/src/hgl.utility.cs
@@ -1,9 +1,51 @@
 partial class hgl {
-    // glDispatchCompute
-    // glDispatchComputeIndirect
-    // glGetGraphicsResetStatus
-    // glGetInternalformat
-    // glGetMultisample
-    // glGetString
-    // glMemoryBarrier
+    public static void DispatchCompute(int groupCountX, int groupCountY, int groupCountZ) {
+        unsafe { gl.Functions.glDispatchCompute((GLuint)groupCountX, (GLuint)groupCountY, (GLuint)groupCountZ); }
+    }
+
+    public static void DispatchComputeIndirect(int indirect) {
+        unsafe { gl.Functions.glDispatchComputeIndirect((GLintptr)indirect); }
+    }
+
+    public static GraphicsResetStatus GetGraphicsResetStatus() {
+        unsafe { return gl.Functions.glGetGraphicsResetStatus(); }
+    }
+
+    public static void GetInternalFormat(TextureTarget target, InternalFormat internalFormat, InternalFormatParameter pname, int[] parameters) {
+        unsafe { fixed (GLint* pParams = parameters) gl.Functions.glGetInternalformativ(target, internalFormat, pname, parameters.Length, pParams); }
+    }
+
+    public static void GetInternalFormat(TextureTarget target, InternalFormat internalFormat, InternalFormatParameter pname, long[] parameters) {
+        unsafe { fixed (GLint64* pParams = parameters) gl.Functions.glGetInternalformati64v(target, internalFormat, pname, parameters.Length, pParams); }
+    }
+
+    public static string GetString(GLString name) {
+        unsafe { return Marshal.PtrToStringUTF8((IntPtr)gl.Functions.glGetString(name)); }
+    }
+
+    public static string GetString(GLString name, int index) {
+        unsafe { return Marshal.PtrToStringUTF8((IntPtr)gl.Functions.glGetStringi(name, (GLuint)index)); }
+    }
+
+    public static void GetMultisample(GLuint index, GLfloat[] value) {
+        unsafe { fixed (GLfloat* ptr = value) { gl.Functions.glGetMultisamplefv(MultisampleParameter.SamplePosition, index, ptr); } }
+    }
+
+    public static void GetMultisample(GLuint index, out GLfloat x, out GLfloat y) {
+        x = y = default;
+        unsafe { fixed (GLfloat* ptr = &x) { gl.Functions.glGetMultisamplefv(MultisampleParameter.SamplePosition, index, ptr); } }
+    }
+
+    public static void GetMultisample(GLuint index, out ValueTuple<GLfloat, GLfloat> value) {
+        value = default;
+        unsafe { fixed (GLfloat* ptr = &value.Item1) { gl.Functions.glGetMultisamplefv(MultisampleParameter.SamplePosition, index, ptr); } }
+    }
+
+    public static void MemoryBarrier(MemoryBarrierMask barriers) {
+        unsafe { gl.Functions.glMemoryBarrier(barriers); }
+    }
+
+    public static void MemoryBarrierByRegion(MemoryBarrierMask barriers) {
+        unsafe { gl.Functions.glMemoryBarrierByRegion(barriers); }
+    }
 }
