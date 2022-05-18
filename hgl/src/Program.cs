a683944fccb28e7d4cf02fc6ebad744169874710
defaultex <default_ex@live.com>
2022-05-18 12:22:36 -0400

    Added a benchmarking function to the hgl project. Finished out the transform feedback implementation for now.

--- hgl/src/Program.cs
+++ hgl/src/Program.cs
@@ -1,6 +1,131 @@
 
 class Program {
     static void Main(string[] args) {
-        
+        Test_ArrayToPointerCopy(10000);
     }
+
+    static void Test_ArrayToPointerCopy(int iterations) {
+#pragma warning disable CA2014 // warning to not use stackalloc inside a loop, we will be doing so anyway
+        RunBenchmark<Action>(Technique_BufferMemoryCopy, iterations);
+        RunBenchmark<Action>(Technique_ArrayCopy, iterations);
+        RunBenchmark<Action>(Technique_MarshalCopy, iterations);
+#pragma warning disable CA2014
+    }
+
+    static void Technique_BufferMemoryCopy() {
+        int count = SampleSet.Length;
+        unsafe {
+            GLchar** ppArray = stackalloc GLchar*[count];
+            for (int i = 0; i < count; i++) {
+                string current = SampleSet[i];
+                int length = current.Length;
+
+                GLchar* pStr = stackalloc GLchar[length];
+                byte[] bStr = Encoding.UTF8.GetBytes(current);
+                fixed (GLchar* vStr = bStr) {
+                    System.Buffer.MemoryCopy(vStr, pStr, bStr.Length, bStr.Length);
+                }
+            }
+        }
+    }
+
+    static void Technique_ArrayCopy() {
+        int count = SampleSet.Length;
+        unsafe {
+            GLchar** ppArray = stackalloc GLchar*[count];
+            for (int i = 0; i < count; i++) {
+                string current = SampleSet[i];
+                int length = current.Length;
+
+                GLchar* pStr = stackalloc GLchar[length];
+                byte[] bStr = Encoding.UTF8.GetBytes(current);
+                for (int j = 0; j < bStr.Length; j++) {
+                    pStr[j] = bStr[j];
+                }
+            }
+        }
+    }
+
+    static void Technique_MarshalCopy() {
+        int count = SampleSet.Length;
+        unsafe {
+            GLchar** ppArray = stackalloc GLchar*[count];
+            for (int i = 0; i < count; i++) {
+                string current = SampleSet[i];
+                int length = current.Length;
+
+                GLchar* pStr = stackalloc GLchar[length];
+                byte[] bStr = Encoding.UTF8.GetBytes(current);
+                Marshal.Copy(bStr, 0, (IntPtr)pStr, bStr.Length);
+            }
+        }
+    }
+
+    static void RunBenchmark<T>(T function, int iterations) where T : Delegate {
+        Action func = function as Action; // pre-cast since it cost about 3um to cast this
+
+        Stopwatch sw = Stopwatch.StartNew();
+        for (int i = 0; i < iterations; i++) {
+            func();
+        }
+        sw.Stop();
+
+        Console.WriteLine($"Test completed in {sw.ElapsedMilliseconds}ms for {iterations} number of iterations for {function.Method.Name}.");
+        Console.WriteLine($"Average of {sw.ElapsedMilliseconds / (double)iterations}ms per iteration.");
+    }
+
+    // just random data to test
+    static readonly string[] SampleSet = new[] {
+        "Boolean",
+        "handleARB",
+        "String",
+        "vdpauSurfaceNV",
+        "Half16NV",
+        "WinCoord",
+        "CoordF",
+        "ClampedFixed",
+        "BufferOffset",
+        "BufferSize",
+        "BufferOffsetARB",
+        "CoordD",
+        "ColorF",
+        "BufferSizeARB",
+        "List",
+        "DrawBufferName",
+        "ClampedFloat32",
+        "MaskedColorIndexValueF",
+        "StencilValue",
+        "ColorB",
+        "ColorD",
+        "ColorI",
+        "ColorS",
+        "ColorUB",
+        "ColorUI",
+        "ColorUS",
+        "CheckedFloat32",
+        "CheckedInt32",
+        "CompressedTextureARB",
+        "Path",
+        "PathElement",
+        "FenceNV",
+        "ClampedFloat64",
+        "BooleanPointer",
+        "FeedbackElement",
+        "PathCommand",
+        "ProgramCharacterNV",
+        "MaskedColorIndexValueI",
+        "ColorIndexValueD",
+        "ColorIndexValueF",
+        "ColorIndexValueI",
+        "ColorIndexValueS",
+        "ColorIndexValueUB",
+        "MaskedStencilValue",
+        "LineStipple",
+        "SelectName",
+        "CoordI",
+        "CoordS",
+        "ClampedStencilValue",
+        "ReplacementCodeSUN",
+        "SampleMaskNV",
+    };
 }
--- hgl/src/hgl.transform_feedback.cs
+++ hgl/src/hgl.transform_feedback.cs
@@ -4,20 +4,73 @@ partial class hgl {
         unsafe { gl.Functions.glBeginTransformFeedback(mode); }
     }
 
-    // glBindTransformFeedback
-    // glCreateTransformFeedbacks
-    // glDeleteTransformFeedbacks
-    // glDrawTransformFeedback
-    // glDrawTransformFeedbackInstanced
-    // glDrawTransformFeedbackStream
-    // glDrawTransformFeedbackStreamInstanced
+    public static void BindTransformFeedback(BindTransformFeedbackTarget target, GLtransformfeedback xfb) {
+        unsafe { gl.Functions.glBindTransformFeedback(target, xfb); }
+    }
+
+    public static void CreateTransformFeedbacks(GLtransformfeedback[] xfbs) {
+        unsafe { fixed (GLtransformfeedback* pIds = xfbs) gl.Functions.glCreateTransformFeedbacks(xfbs.Length, pIds); }
+    }
+
+    public static void CreateTransformFeedbacks(out GLtransformfeedback xfb) {
+        unsafe { fixed (GLtransformfeedback* pIds = &xfb) gl.Functions.glCreateTransformFeedbacks(1, pIds); }
+    }
+
+    public static void DeleteTransformFeedbacks(params GLtransformfeedback[] xfbs) {
+        unsafe { fixed (GLtransformfeedback* pIds = xfbs) gl.Functions.glDeleteTransformFeedbacks(xfbs.Length, pIds); }
+    }
+
+    public static void DrawTransformFeedback(PrimitiveType mode, GLtransformfeedback xfb) {
+        unsafe { gl.Functions.glDrawTransformFeedback(mode, xfb); }
+    }
+
+    public static void DrawTransformFeedbackInstanced(PrimitiveType mode, GLtransformfeedback xfb, int instanceCount) {
+        unsafe { gl.Functions.glDrawTransformFeedbackInstanced(mode, xfb, instanceCount); }
+    }
+
+    public static void DrawTransformFeedbackStream(PrimitiveType mode, GLtransformfeedback xfb, int stream) {
+        unsafe { gl.Functions.glDrawTransformFeedbackStream(mode, xfb, (GLuint)stream); }
+    }
+
+    public static void DrawTransformFeedbackStream(PrimitiveType mode, GLtransformfeedback xfb, int stream, int instanceCount) {
+        unsafe { gl.Functions.glDrawTransformFeedbackStreamInstanced(mode, xfb, (GLuint)stream, instanceCount); }
+    }
 
     public static void EndTransformFeedback() {
         unsafe { gl.Functions.glEndTransformFeedback(); }
     }
 
-    // glGenTransformFeedbacks
-    // glGetTransformFeedback
+    public static void GenTransformFeedbacks(GLtransformfeedback[] xfbs) {
+        unsafe { fixed (GLtransformfeedback* pIds = xfbs) gl.Functions.glGenTransformFeedbacks(xfbs.Length, pIds); }
+    }
+
+    public static void GenTransformFeedbacks(out GLtransformfeedback xfbs) {
+        unsafe { fixed (GLtransformfeedback* pIds = &xfbs) gl.Functions.glGenTransformFeedbacks(1, pIds); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfb, TransformFeedbackParameter pname, int[] parameters) {
+        unsafe { fixed (GLint* pParams = parameters) gl.Functions.glGetTransformFeedbackiv(xfb, pname, pParams); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfd, TransformFeedbackParameter pname, out int parameters) {
+        unsafe { fixed (GLint* pParams = &parameters) gl.Functions.glGetTransformFeedbackiv(xfd, pname, pParams); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfb, TransformFeedbackParameter pname, int index, int[] parameters) {
+        unsafe { fixed (GLint* pParams = parameters) gl.Functions.glGetTransformFeedbacki_v(xfb, pname, (GLuint)index, pParams); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfd, TransformFeedbackParameter pname, int index, out int parameters) {
+        unsafe { fixed (GLint* pParams = &parameters) gl.Functions.glGetTransformFeedbacki_v(xfd, pname, (GLuint)index, pParams); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfb, TransformFeedbackParameter pname, int index, long[] parameters) {
+        unsafe { fixed (GLint64* pParams = parameters) gl.Functions.glGetTransformFeedbacki64_v(xfb, pname, (GLuint)index, pParams); }
+    }
+
+    public static void GetTransformFeedback(this GLtransformfeedback xfd, TransformFeedbackParameter pname, int index, out long parameters) {
+        unsafe { fixed (GLint64* pParams = &parameters) gl.Functions.glGetTransformFeedbacki64_v(xfd, pname, (GLuint)index, pParams); }
+    }
 
     public static void GetTransformFeedbackVarying(GLprogram program, int index, int bufSize, out GLsizei size, out AttributeType type, out string name) {
         unsafe {
@@ -30,22 +83,40 @@ partial class hgl {
         }
     }
 
-    // glIsTransformFeedback
-    // glPauseTransformFeedback
-    // glResumeTransformFeedback
-    // glTransformFeedbackBufferBase
-    // glTransformFeedbackBufferRange
+    public static bool IsTransformFeedback(this GLtransformfeedback xfb) {
+        unsafe { return (gl.Functions.glIsTransformFeedback(xfb) != gl.Constants.GL_FALSE); }
+    }
 
-    // TODO : validate this method actually works, have a strong feeling it doesn't
-    public static void glTransformFeedbackVaryings(GLprogram program, int count, string[] varyings, TransformFeedbackBufferMode bufferMode) {
+    public static void PauseTransformFeedback() {
+        unsafe { gl.Functions.glPauseTransformFeedback(); }
+    }
+
+    public static void ResumeTransformFeedback() {
+        unsafe { gl.Functions.glResumeTransformFeedback(); }
+    }
+
+    public static void TransformFeedbackBufferBase(this GLtransformfeedback xfb, int index, GLbuffer buffer) {
+        unsafe { gl.Functions.glTransformFeedbackBufferBase(xfb, (GLuint)index, buffer); }
+    }
+
+    public static void TransformFeedbackBufferRange(this GLtransformfeedback xfb, int index, GLbuffer buffer, int offset, int size) {
+        unsafe { gl.Functions.glTransformFeedbackBufferRange(xfb, (GLuint)index, buffer, (GLintptr)offset, (GLsizeiptr)size); }
+    }
+
+    public static void TransformFeedbackVaryings(GLprogram program, int count, string[] varyings, TransformFeedbackBufferMode bufferMode) {
         unsafe {
-            GLchar** varray = stackalloc GLchar*[varyings.Length];
+            GLchar** ppArray = stackalloc GLchar*[varyings.Length];
             for (int i = 0; i < varyings.Length; i++) {
-                // note: doing a stackalloc here says 'unable to convert byte* to byte*' as there are two unique 'byte*' types
-                byte[] vstr = Encoding.UTF8.GetBytes(varyings[i]);
-                fixed (GLchar* vstrPtr = vstr) varray[i] = vstrPtr;
+#pragma warning disable CA2014
+                GLchar* pStr = stackalloc GLchar[varyings[i].Length];
+#pragma warning restore CA2014
+                byte[] bStr = Encoding.UTF8.GetBytes(varyings[i]);
+                fixed (GLchar* vStr = bStr) {
+                    System.Buffer.MemoryCopy(vStr, pStr, bStr.Length, bStr.Length);
+                }
+                ppArray[i] = pStr;
             }
-            gl.Functions.glTransformFeedbackVaryings(program, (GLsizei)count, varray, bufferMode);
+            gl.Functions.glTransformFeedbackVaryings(program, (GLsizei)count, ppArray, bufferMode);
         }
     }
 }
